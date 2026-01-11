4ea4f9f6eaa398f815a9a976cc0990d893d1f206
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-11 18:33:59 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/App.java
+++ src/application/App.java
@@ -2,13 +2,20 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+
         Department  obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", LocalDate.now(), 2300.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(obj);
         System.out.println(seller.toString());
     }
