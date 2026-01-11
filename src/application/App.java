90ea83e5575ab5d3402d732c03bfc2fc2434a004
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-11 18:15:00 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department  obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", LocalDate.now(), 2300.00, obj);
         System.out.println(obj);
+        System.out.println(seller.toString());
     }
 }
