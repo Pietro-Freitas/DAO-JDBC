039538120661b06a171a6918850596f2e20e2e24
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-12 19:48:33 -0300

    create DepartmentDaoJDBC and findById implementation

---
+++ src/application/App2.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class App2 {
+    public static void main(String[] args) {
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== Test 1: department findById");
+        Department department = departmentDao.findById(2);
+        System.out.println(department);
+
+    }
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,65 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao{
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department department) {
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Department department) {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer Id) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Department findById(Integer Id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("SELECT * FROM department WHERE id = ?");
+            st.setInt(1, Id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department department = new Department();
+                department.setId(rs.getInt("Id"));
+                department.setName(rs.getString("Name"));
+                return department;
+            }
+            return null;
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultset(rs);
+            DB.closeConnection();
+        }
+    }
+
+    @Override
+    public List<Department> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+    
+}
